0a9d64152611425ead409d8283853488768ca7a6
itay-cyber <57634628+itay-cyber@users.noreply.github.com>
2020-10-01 15:23:51 +0200

    Fixed simon says function
    Added else statement for if no wakeword said.
    changed wakeword back to Bobby

--- main.py
+++ main.py
@@ -25,13 +25,12 @@ def onVoiceEnter(txtArr):
         try:
             #simon says func
             if(txtArr[1] == "simon" and txtArr[2] == "says"):
-                voiceEngine.tts("simon says") 
-                joinedBoi =  txtArr.join(" ").replace(voiceEngine.wakeword, "").replace("simon", "").replace("says", "")
+                joinedBoi =  " ".join(txtArr).replace(voiceEngine.wakeword, "").replace("simon", "").replace("says", "")
                 voiceEngine.tts(joinedBoi)
                 
             
             #open google func
-            elif (txtArr[1] == "open" txtArr[2] == "browser"):
+            elif (txtArr[1] == "open" and txtArr[2] == "browser"):
                 try:
                     webbrowser.open("https://www.google.com")
                     voiceEngine.tts("Opened browser successfully")
@@ -41,10 +40,11 @@ def onVoiceEnter(txtArr):
                 
             
 
-        except:
+        except Exception as e:
             voiceEngine.tts("I have stupid. Something went wrong.")
-
+            print(e)
+    else:
+        print("no wakework said")
 
 
 onVoiceEnter(voiceEngine.recognize())
-
