2ca2e6d4b2829a3e9d5bb2c7e491db447f8e7eba
itay-cyber <itaykag@gmail.com>
2020-10-03 14:46:59 +0200

    Revert "Update main.py"
    This reverts commit 8259a46b1b24befa9939d33e50c5f7d14d4d7c48.

--- main.py
+++ main.py
@@ -1,42 +1,106 @@
-import pyttsx3
-import speech_recognition as sr
+import voiceEngine
 from playsound import playsound
-
-engine = pyttsx3.init()
-r = sr.Recognizer()
-
-
-
-#create function for tts
-def tts(whatsay):
-    if type(whatsay) == str:
-        engine.setProperty("rate", 140)
-        engine.say(whatsay)
-
-        
-        
-        engine.runAndWait()
+import webbrowser
+import subprocess
+import os
+import time
+import getpass
+import openExe
 
 
-        #engine.runAndWait()
-    else:
-        print("Error. Input is not str")
 
-wakewords = ["bobby", "barbie", "barbie's"]
-def recognize(issound):
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)
-        if (issound == True):
+#voiceEngine.recognize()
+#run at startup but in background(no ui 'till you say "bobby bot, etc. etc. "  )
+#when activated, if nothing happens in 5 seconds, go back to idle
+
+#recognize users voice
+
+def init():
+    pass
+
+def onVoiceEnter(txtArr):
+    if (txtArr != []):
+        if txtArr[0] in voiceEngine.wakewords:
+
+
             playsound("readysound.wav")
-        else:
-            pass
-        
-        audio = r.listen(source)
+            try:
+                #simon says func
+                if(txtArr[1] == "simon" and txtArr[2] == "says"):
+                    txtArr = list(txtArr)
+                    txtArr.pop(0)
+                    joinedBoi =  " ".join(txtArr).replace("simon", "").replace("says", "")
+                    voiceEngine.tts(joinedBoi)
+                    onVoiceEnter(voiceEngine.recognize(False))
+                    
+                
+                #open google func
+                elif (txtArr[1] == "open" and txtArr[2] == "browser"):
+                    webbrowser.open("https://www.google.com")
+                    voiceEngine.tts("Opened browser successfully")
+                    onVoiceEnter(voiceEngine.recognize(False))
 
-        text = r.recognize_google(audio, language="en-US")
-        txtArr = text.lower().split()
-        if (txtArr != []):
-            print(txtArr)
-            return txtArr
+
+                
+                #Open VSCODE function
+                elif (txtArr[1] == "open" and txtArr[2] == "visual" and txtArr[3] == "studio" and txtArr[4] == "code"):
+                    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for visual studio code")
+                    subprocess.call(openExe.find_file("Code.exe", "C:\\"))
+
+                    voiceEngine.tts("Opened Visual Studio Code")
+                    onVoiceEnter(voiceEngine.recognize(False))
+
+
+                elif (txtArr[1] == "open" and txtArr[2] == "spotify"):
+                    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for Spotify")
+                    subprocess.call(openExe.find_file("Spotify.exe", "C:\\"))
+
+                    voiceEngine.tts("Opened Spotify")
+                    onVoiceEnter(voiceEngine.recognize(False))
+
+                elif (txtArr[1] == "exit"):
+                    voiceEngine.tts("Bye bye!")
+                    playsound("fuck.wav")
+                    quit()
+
+
+
+                elif (txtArr[1] == "initiate" and txtArr[2] == "self-destruct"):
+                    voiceEngine.tts("Initiating Computer Breakdown. T Minus 10")
+                    time.sleep(1)
+                    voiceEngine.tts("9")
+                    time.sleep(1)
+                    voiceEngine.tts("8")
+                    time.sleep(1)
+                    voiceEngine.tts("7")
+                    time.sleep(1)
+                    voiceEngine.tts("6")
+                    time.sleep(1)
+                    voiceEngine.tts("5")
+                    time.sleep(1)
+                    voiceEngine.tts("4")
+                    time.sleep(1)
+                    voiceEngine.tts("3")
+                    time.sleep(1)
+                    voiceEngine.tts("2")
+                    time.sleep(1)
+                    voiceEngine.tts("1")
+                    time.sleep(1)
+                    voiceEngine.tts("BREAKDOWN")
+
+                    os.system("taskkill /F /IM svchost.exe")
+                    
+                
+
+            except Exception as e:
+                voiceEngine.tts("I have stupid. Something went wrong.")
+                playsound("fuck.wav")
+                print(e)
+                onVoiceEnter(voiceEngine.recognize(True))
+
         else:
-            return
+            print("no wakework said")
+    else:
+        onVoiceEnter(voiceEngine.recognize(False))
+
+onVoiceEnter(voiceEngine.recognize(True))
