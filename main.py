184f05b3ef585da82831861f3fbbdbbc2d95d42c
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-10-04 16:55:05 +0200

    cleaned up main.py(it was a pain pls put new code into functions)

--- main.py
+++ main.py
@@ -10,11 +10,92 @@ from pynput.keyboard import Key, Controller
 
 
 
-#voiceEngine.recognize()
-#run at startup but in background(no ui 'till you say "bobby bot, etc. etc. "  )
-#when activated, if nothing happens in 5 seconds, go back to idle
 
-#recognize users voice
+def SimonSays(txtArr):
+    txtArr = list(txtArr)
+    txtArr.pop(0)
+    joinedBoi =  " ".join(txtArr).replace("simon", "").replace("says", "")
+    voiceEngine.tts(joinedBoi)
+    onVoiceEnter(voiceEngine.recognize(False))
+    
+
+
+
+
+
+
+def OpenBrowser():
+    webbrowser.open("https://www.google.com")
+    voiceEngine.tts("Opened browser successfully")
+    onVoiceEnter(voiceEngine.recognize(False))
+    
+
+
+def OpenVSCode():
+    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for visual studio code")
+    subprocess.call(openExe.find_file("Code.exe", "C:\\"))
+    voiceEngine.tts("Opened Visual Studio Code")
+    onVoiceEnter(voiceEngine.recognize(False))
+
+
+def OpenSpotify():
+    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for Spotify")
+    subprocess.call(openExe.find_file("Spotify.exe", "C:\\"))
+
+    voiceEngine.tts("Opened Spotify")
+    onVoiceEnter(voiceEngine.recognize(False))
+
+
+
+def SelfDistruct():
+    voiceEngine.tts("Initiating Computer Breakdown. T Minus 10")
+    time.sleep(1)
+    voiceEngine.tts("9")
+    time.sleep(1)
+    voiceEngine.tts("8")
+    time.sleep(1)
+    voiceEngine.tts("7")
+    time.sleep(1)
+    voiceEngine.tts("6")
+    time.sleep(1)
+    voiceEngine.tts("5")
+    time.sleep(1)
+    voiceEngine.tts("4")
+    time.sleep(1)
+    voiceEngine.tts("3")
+    time.sleep(1)
+    voiceEngine.tts("2")
+    time.sleep(1)
+    voiceEngine.tts("1")
+    time.sleep(1)
+    voiceEngine.tts("BREAKDOWN")
+
+    os.system("taskkill /F /IM svchost.exe")
+
+def exitProg():
+    voiceEngine.tts("Bye bye!")
+    playsound("fuck.wav")
+    quit()
+
+def search(txtArr):
+    txtArr = list(txtArr)
+    txtArr.pop(0)
+    joinedBoi =  " ".join(txtArr).replace("search", "")
+    webbrowser.open_new_tab("https://www.google.com/search?q=" + joinedBoi)
+
+def Type(txtArr):
+    txtArr = list(txtArr)
+    txtArr.pop(0)
+    joinedBoi =  " ".join(txtArr).replace("type", "")
+    keyboard.type(joinedBoi)
+
+
+
+
+
+
+
+
 
 keyboard = Controller()
 
@@ -31,80 +112,42 @@ def onVoiceEnter(txtArr):
 
                 #simon says func
                 if(txtArr[1] == "simon" and txtArr[2] == "says"):
-                    txtArr = list(txtArr)
-                    txtArr.pop(0)
-                    joinedBoi =  " ".join(txtArr).replace("simon", "").replace("says", "")
-                    voiceEngine.tts(joinedBoi)
-                    onVoiceEnter(voiceEngine.recognize(False))
+                    SimonSays(txtArr)
+ 
                     
                 
                 #open google func
                 elif (txtArr[1] == "open" and txtArr[2] == "browser"):
-                    webbrowser.open("https://www.google.com")
-                    voiceEngine.tts("Opened browser successfully")
-                    onVoiceEnter(voiceEngine.recognize(False))
+                    OpenBrowser()
+                  
 
 
                 
                 #Open VSCODE function
                 elif (txtArr[1] == "open" and txtArr[2] == "visual" and txtArr[3] == "studio" and txtArr[4] == "code"):
-                    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for visual studio code")
-                    subprocess.call(openExe.find_file("Code.exe", "C:\\"))
-
-                    voiceEngine.tts("Opened Visual Studio Code")
-                    onVoiceEnter(voiceEngine.recognize(False))
+                    OpenVSCode()
 
 
                 elif (txtArr[1] == "open" and txtArr[2] == "spotify"):
-                    voiceEngine.tts("Sorry, this might take some time. Scanning your computer for Spotify")
-                    subprocess.call(openExe.find_file("Spotify.exe", "C:\\"))
-
-                    voiceEngine.tts("Opened Spotify")
-                    onVoiceEnter(voiceEngine.recognize(False))
+                    OpenSpotify
 
                 elif (txtArr[1] == "exit"):
-                    voiceEngine.tts("Bye bye!")
-                    playsound("fuck.wav")
-                    quit()
+                    exitProg()
+
 
 
 
                 elif (txtArr[1] == "initiate" and txtArr[2] == "self-destruct"):
-                    voiceEngine.tts("Initiating Computer Breakdown. T Minus 10")
-                    time.sleep(1)
-                    voiceEngine.tts("9")
-                    time.sleep(1)
-                    voiceEngine.tts("8")
-                    time.sleep(1)
-                    voiceEngine.tts("7")
-                    time.sleep(1)
-                    voiceEngine.tts("6")
-                    time.sleep(1)
-                    voiceEngine.tts("5")
-                    time.sleep(1)
-                    voiceEngine.tts("4")
-                    time.sleep(1)
-                    voiceEngine.tts("3")
-                    time.sleep(1)
-                    voiceEngine.tts("2")
-                    time.sleep(1)
-                    voiceEngine.tts("1")
-                    time.sleep(1)
-                    voiceEngine.tts("BREAKDOWN")
-
-                    os.system("taskkill /F /IM svchost.exe")
+                    SelfDistruct()
+                    
 
                 elif(txtArr[1] == "search"):
-                    txtArr = list(txtArr)
-                    txtArr.pop(0)
-                    joinedBoi =  " ".join(txtArr).replace("search", "")
-                    webbrowser.open_new_tab("https://www.google.com/search?q=" + joinedBoi)
+                    search(txtArr)
+
 
                 elif(txtArr[1] == "type"):
-                    txtArr = list(txtArr)
-                    txtArr.pop(0)
-                    joinedBoi =  " ".join(txtArr).replace("type", "")
-                    keyboard.type(joinedBoi)
+                    Type(txtArr)
+
                     
 
 
